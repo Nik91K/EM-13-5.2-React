447bfa2cc3366eb248cabd7fa950b1ba42341b27
progenitor <korndavid10@gmail.com>
2025-08-14 19:19:42 +0300

    forth commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path = '/' element = {App}/>
+
+    </Routes>
+  </BrowserRouter>
 )
