4675b284574d29a7c46f457f870b3be6efd07bde
progenitor <korndavid10@gmail.com>
2025-08-15 13:06:42 +0300

    chan

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
   return (
     <>
     <LayoutPage>
-      <img src='https://i.pinimg.com/236x/c0/6b/89/c06b89b3b9aaf19513565e41123c5802.jpg'></img>
+      <img src='https://i.pinimg.com/1200x/70/f9/86/70f986ee7e202d16338dc44ff1fbb68f.jpg' style={{ width: "500px", height: "500px", marginLeft: "50px"}}></img>
+      <img src='../src/assets/cat.gif' alt="Котик" style={{ width: "500px", height: "500px" }} />
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmQ_fZV_3jrOYcAaJrkjFVztYSTLIN_2PO29Ty7luWZFmksLIaHYwHoW09ykfL-Rp0OiY&usqp=CAU' style={{ width: "500px", height: "500px" }}></img>
     </LayoutPage>
     </>
   )
