1fa829a78c25ff97274ff04dd20d06cd56fb09ad
Your Name <you@example.com>
2025-08-15 13:11:00 +0300

    ApdateStyleImg

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <LayoutPage>
-      <img src='https://i.pinimg.com/1200x/70/f9/86/70f986ee7e202d16338dc44ff1fbb68f.jpg' style={{ width: "500px", height: "500px", marginLeft: "50px"}}></img>
+      <img src='https://i.pinimg.com/1200x/70/f9/86/70f986ee7e202d16338dc44ff1fbb68f.jpg' style={{ width: "500px", height: "500px", marginLeft: "-80px"}}></img>
       <img src='../src/assets/cat.gif' alt="Котик" style={{ width: "500px", height: "500px" }} />
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmQ_fZV_3jrOYcAaJrkjFVztYSTLIN_2PO29Ty7luWZFmksLIaHYwHoW09ykfL-Rp0OiY&usqp=CAU' style={{ width: "500px", height: "500px" }}></img>
     </LayoutPage>
